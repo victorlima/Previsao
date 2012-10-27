343cfedeb710f074e400d07f12cfda1753c63446
Victor Lima <victorloureirolima@gmail.com>
2012-10-27 17:49:05 -0200

    herokuing

--- previsao/previsao/settings.py
+++ previsao/previsao/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'previsao',                      # Or path to database file if using sqlite3.
+        'NAME': 'previsao.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -152,4 +152,4 @@ LOGGING = {
 import dj_database_url
 import os
 if os.getcwd() == "/app":
-    DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+    DATABASES = {'default': dj_database_url.config(default='postgres://localhost/previsao')}
