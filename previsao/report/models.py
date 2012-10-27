43ba71859d95038e7f8fb363e6081436e5a1900e
Victor Lima <victorloureirolima@gmail.com>
2012-10-27 16:48:40 -0200

    template de chart e unicode nos models

--- previsao/report/models.py
+++ previsao/report/models.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from django.db import models
 
 DIRECTIONS = (
@@ -20,20 +21,35 @@ class ReportDay( models.Model ):
 	fivePM = models.ForeignKey('Report', related_name='fivePM_report')
 	eightPM = models.ForeignKey('Report', related_name='eightPM_report')
 
+	def __unicode__(self):
+		return unicode(self.date)
+
 class Report( models.Model ):
 	waveReport = models.ForeignKey('WaveReport')
 	windReport = models.ForeignKey('WindReport')
 	tideReport = models.ForeignKey('TideReport')
+
+	def __unicode__(self):
+		return "Wave: %s(m), %s" % (self.waveReport.height, self.waveReport.direction )
 
 class WaveReport( models.Model ):
 	direction = models.CharField(max_length = 3, choices = DIRECTIONS )
 	height = models.CharField( max_length = 3 )
 	period = models.CharField( max_length = 3 )
 
+	def __unicode__(self):
+		return "Wave: %s(m), %s, %s" % (self.height, self.direction, self.period )
+
+
 class WindReport( models.Model ):
 	direction = models.CharField(max_length = 3, choices = DIRECTIONS )
  	intensity = models.CharField( max_length = 3 )
 
+ 	def __unicode__(self):
+ 		return "Wind: %s (nos), %s" % ( self.intensity, self.direction )
+
 class TideReport( models.Model ):
 	height = models.CharField( max_length = 3 )
 
+ 	def __unicode__(self):
+ 		return "Tide: %s (m)" % ( self.height )
